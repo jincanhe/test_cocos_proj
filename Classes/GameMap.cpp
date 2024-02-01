1cb8e2088e252a3c3726a4809ca1d2ceb6172ac9
598224311@qq.com <dgt742cv>
2024-02-01 23:57:28 +0800

    寻路

--- Classes/GameMap.cpp
+++ Classes/GameMap.cpp
@@ -5,28 +5,65 @@
 #include "GameMap.h"
 
 #include "ui/UIImageView.h"
+// MAP_VOID,
+// MAP_POS,
+// MAP_ROAD,
+// MAP_GLASS,
+std::vector<int> costMap =
+{
+    10,
+    255,
+    12,
+    15,
+};
 
 void GameMap::setBlocks() {
-    width = 15;
-    height = 500;
+    width = 6; ///////
+    height = 3;
 
     auto dataSize = width * height * sizeof(MAPBLOCK);
     // mapBlocks = (MAPBLOCK*)malloc(dataSize);
-    mapBlocks = new MAPBLOCK[ width * height]();
+    mapBlocks = new MAPBLOCK[width * height]();
 
     auto size = sizeof(MAPBLOCK);
-    for (size_t i = 0; i < dataSize / sizeof(MAPBLOCK); i++) {
+    for (size_t i = 0; i < dataSize / sizeof(MAPBLOCK); i++)
+    {
         mapBlocks[i].inpassable = 0;
         mapBlocks[i].blockType = BlockType::MAP_VOID;
-        mapBlocks[i].lock = 1;
+        mapBlocks[i].cost = 1;
 
-        if(i == 2 || i == 4 || i == 13 || i == 25 || i == 45 || i == 55 || i == 47) {
+        if (i == 2 || i == 4 || i == 13 || i == 25 || i == 45 || i == 55 || i == 47)
+        {
             mapBlocks[i].blockType = BlockType::MAP_POS;
             mapBlocks[i].inpassable = 1;
-            mapBlocks[i].lock = 0;
+            mapBlocks[i].cost = 0;
+        }
+        // else
+        // {
+        //     if(i % 3 == 2)
+        //     {
+        //         mapBlocks[i].blockType = BlockType::MAP_ROAD;
+        //     }
+        // }
+
+
+        if (i % 3 == 1)
+        {
+            mapBlocks[i].blockType = BlockType::MAP_ROAD;
+        }
+
+        if( i == 11)
+        {
+            mapBlocks[i].blockType = BlockType::MAP_ROAD;
+
+        }
+
+        if( i == 16)
+        {
+            mapBlocks[i].blockType = BlockType::MAP_VOID;
+
         }
     }
-
 }
 
 void GameMap::bindEvents() {
@@ -34,25 +71,36 @@ void GameMap::bindEvents() {
     btn_1->loadTexture("image/maingame/btn_common_m1.png");
     addChild(btn_1);
     btn_1->setScale(0.5);
-    btn_1->setPosition(Vec2(32 * (width+2),-50));
+    btn_1->setPosition(Vec2(32 * (width + 2), -50));
     btn_1->setTouchEnabled(true);
-    btn_1->addTouchEventListener([=](Ref* sender, ui::Widget::TouchEventType type)
-    {
+    btn_1->addTouchEventListener([=](Ref* sender, ui::Widget::TouchEventType type) {
         if (type == ui::Widget::TouchEventType::ENDED)
         {
             clearHeatMap();
-            generateHeatMap(0,0);
+            generateHeatMap(2, 1);
+            generateVectorMap(1);
+            drawHeatMap();
         }
     });
 }
 
+void GameMap::initUI() {
+    tileIndicator = DrawNode::create();
+    addChild(tileIndicator, 65532);
+    debugDrawNode = DrawNode::create();
+    addChild(debugDrawNode, 65532);
+    heatDebugNode = Node::create();
+    addChild(heatDebugNode, 65535);
+}
+
 bool GameMap::init() {
+
+    initUI();
     setBlocks();
     drawMap();
     bindEvents();
 
     return true;
-
 }
 
 GameMap::MAPBLOCK* GameMap::getMapBlock(int x, int y) {
@@ -63,37 +111,104 @@ GameMap::MAPBLOCK* GameMap::getMapBlock(int x, int y) {
 }
 
 void GameMap::drawMap() {
-    tileIndicator = DrawNode::create();
-    addChild(tileIndicator, 65532);
-    heatDebugNode = Node::create();
-    addChild(heatDebugNode, 65533);
-
-
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
             Color4F iColor;
             Vec2 pt1(x * 32.0f, y * 32.0f * -1.0f);
             Vec2 pt2(pt1.x + 32.0f, pt1.y - 32.0f);
 
             MAPBLOCK* block = getMapBlock(x, y);
-            tileIndicator->drawRect(pt1, pt2, Color4F(.0f, 0.2f, 0.5f, 0.6f));
+            tileIndicator->drawRect(pt1, pt2, Color4F(.0f, 0.2f, 0.5f, 0.3f));
 
-            if(getMapBlock(x,y)->inpassable == 1) {
-                tileIndicator->drawSolidRect(Vec2(pt1.x,pt1.y), Vec2(pt2.x,pt2.y), Color4F(1.0f, 0.2f, 0.5f, 0.6f));
+            if (block->inpassable == 1)
+            {
+                tileIndicator->drawSolidRect(Vec2(pt1.x, pt1.y), Vec2(pt2.x, pt2.y), Color4F(0.5f, 0.2f, 0.5f, 0.3f));
+            } else
+            {
+                if (block->blockType == BlockType::MAP_ROAD)
+                {
+                    auto btn_1 = ui::ImageView::create();
+                    btn_1->loadTexture("image/maingame/road.png");
+                    addChild(btn_1);
+                    btn_1->setPosition(pt1 + Vec2(16.f, -16.f));
+                    btn_1->setOpacity(128);
+                    btn_1->setScale(0.7);
+                }
+
+                if (block->blockType == BlockType::MAP_GLASS)
+                {
+                    auto btn_1 = ui::ImageView::create();
+                    btn_1->loadTexture("image/maingame/glass.png");
+                    addChild(btn_1);
+                    btn_1->setPosition(pt1 + Vec2(16.f, -16.f));
+                    btn_1->setOpacity(128);
+                    btn_1->setScale(0.7);
+                }
             }
         }
     }
 }
 
 void GameMap::clearHeatMap() {
-    if(!flowBlocks)
+    if (!flowBlocks)
     {
         flowBlocks = new FlowBlock[width * height]();
     }
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
             int pos = y * width + x;
             flowBlocks[pos].integration = 65535;
+        }
+    }
+}
+
+void GameMap::drawHeatMap() {
+
+
+    std::vector<Vec2> flowIndex = {
+        Vec2(0.0f, 0.0f),
+        Vec2(-1.0f, -1.0f).getNormalized(),
+        Vec2(0.0f, -1.0f),
+        Vec2(1.0f, -1.0f).getNormalized(),
+        Vec2(1.0f, 0.0f),
+        Vec2(1.0f, 1.0f).getNormalized(),
+        Vec2(0.0f, 1.0f),
+        Vec2(-1.0f, 1.0f).getNormalized(),
+        Vec2(-1.0f, 0.0f),
+    };
+
+
+    heatDebugNode->removeAllChildrenWithCleanup(true);
+    debugDrawNode->removeAllChildrenWithCleanup(true);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Color4F iColor;
+            Vec2 pt1(x * 32.0f, y * 32.0f * -1.0f);
+            Vec2 pt2(pt1.x + 32.0f, pt1.y - 32.0f);
+
+
+            int pos = y * width + x;
+            char buf[256];
+            sprintf(buf, "%s,%d", flowBlocks[pos].integration > 60000
+                                      ? "x"
+                                      : std::to_string(flowBlocks[pos].integration).c_str(),  flowBlocks[pos].flow);
+            auto label_1 = PLabel::createWithTTF(buf, "fonts/SiYuanSongTi.otf", 14);
+            label_1->setPosition(pt1 + Vec2(16.f, -16.f));
+            label_1->setTextColor(Color4B::BLACK);
+            heatDebugNode->addChild(label_1);
+
+            Vec2 origin(pt1 + Vec2(16.f, 16.f));
+            Vec2 dest = origin + flowIndex[flowBlocks[pos].flow] * 16.0f;
+            origin.y *= -1.0f;
+            dest.y *= -1.0f;
+            debugDrawNode->drawLine(origin, dest, Color4F::YELLOW);
+
         }
     }
 }
@@ -102,13 +217,12 @@ void GameMap::clearHeatMap() {
 // 作用: 为终点 一次性生成所有格子的最短路径   todo: 多线程开启
 /////////////////////////////////////////////////////////////////////////////
 void GameMap::generateHeatMap(int startX, int startY) {
-    if(!flowBlocks)
+    if (!flowBlocks)
     {
         flowBlocks = new FlowBlock[width * height]();
     }
 
-
-    //-----  初始化终点
+    //-----  初始化终点  内存视图中为16进制
     std::list<HeatFront> frontQueue;
 
     HeatFront checkPos, checkPosNew, checkPosLeft, checkPosRight, checkPosTop, checkPosDown;
@@ -128,7 +242,6 @@ void GameMap::generateHeatMap(int startX, int startY) {
     int whileCount = 0;
     while (!frontQueue.empty())
     {
-
         checkPos = frontQueue.front();
         frontQueue.pop_front();
         whileCount++;
@@ -136,84 +249,162 @@ void GameMap::generateHeatMap(int startX, int startY) {
         block = &mapBlocks[pos];
         int now = flowBlocks[pos].integration;
 
-        const bool checkBoolList[4] = { checkPos.xy.y > 0, checkPos.xy.y < height -1, checkPos.xy.x > 0, checkPos.xy.x < width - 1};
-        const int blockPosOffsets[4] = { -width, width, -1 ,1};    //  下, 上, 左, 右
-        int checkPosOffsets[4] = { -0x10000, 0x10000, -1, 1};
+        const bool checkBoolList[4] = {
+            checkPos.xy.y > 0, checkPos.xy.y < height - 1, checkPos.xy.x > 0, checkPos.xy.x < width - 1
+        };
+        const int blockPosOffsets[4] = {-width, width, -1, 1}; //  下, 上, 左, 右
+        int checkPosOffsets[4] = {-0x10000, 0x10000, -1, 1};
 
         for (int i = 0; i < 4; i++)
         {
-            if(checkBoolList[i])
+            if (checkBoolList[i])
             {
                 //--- 对应格子的各种信息
                 int newPos = pos + blockPosOffsets[i];
                 block = &mapBlocks[newPos];
-                checkPosNew.xy.value = (uint32_t)((int)checkPos.xy.value + checkPosOffsets[i]);
+                checkPosNew.xy.value = (uint32_t) ((int) checkPos.xy.value + checkPosOffsets[i]);
                 //
 
                 int addVal = 10;
 
-                if(block->inpassable == 1)
+                if (block->inpassable == 1)
                 {
-                    addVal = 5;
+                    continue;
                 }
 
-                int newVal = addVal + now;
+                int newVal = now + costMap[static_cast<int>(block->blockType)];
 
-                if(flowBlocks[newPos].integration == 65535)  //每一个格只处理一次
+                if (flowBlocks[newPos].integration > newVal)
                 {
-                    flowBlocks[newPos].integration = newVal;  //真实存放值
+                    flowBlocks[newPos].integration = newVal; //真实存放值
                     count++;
                     checkPosNew.heat = newVal;
-                    block = &mapBlocks[newPos];
-
 
                     auto itr = frontQueue.begin();
-
-
                     while (itr != frontQueue.end())
                     {
-                        //-- 这里优化了 对于有 不同花费时的循环次数  todo理解
-                          // auto insPos = *itr;
-                          // if((int)insPos.heat > newVal)
-                          // {
-                          //     frontQueue.insert(itr, checkPosNew);
-                          // }
+                        //-- 这里优化了 对于有 不同花费时的循环次数:  对热力值进行排序  优先处理热力值小的
+                        auto insPos = *itr;
+                        if ((int) insPos.heat > newVal)
+                        {
+                            frontQueue.insert(itr, checkPosNew);
+                            break;
+                        }
                         itr++;
                     }
 
 
-                    if(itr == frontQueue.end())
+                    if (itr == frontQueue.end())
                     {
                         frontQueue.push_back(checkPosNew);
                     }
-
                 }
-
             }
         }
     }
 
+    flowBlocks[16].integration = 34;
+    auto countP = PLabel::createWithTTF(std::to_string(count), "fonts/SiYuanSongTi.otf", 18);
+    countP->setPosition(Vec2(32 * (width + 2), -50));
+    countP->setTextColor(Color4B::BLACK);
+    heatDebugNode->addChild(countP);
 
-    heatDebugNode->removeAllChildrenWithCleanup(true);
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            Color4F iColor;
-            Vec2 pt1(x * 32.0f, y * 32.0f * -1.0f);
-            Vec2 pt2(pt1.x + 32.0f, pt1.y - 32.0f);
+    auto wcountP = PLabel::createWithTTF(std::to_string(whileCount), "fonts/SiYuanSongTi.otf", 18);
+    wcountP->setPosition(Vec2(32 * (width + 2), -50 - 32));
+    wcountP->setTextColor(Color4B::BLACK);
+    heatDebugNode->addChild(wcountP);
+}
+
+void GameMap::generateVectorMap(int id) {
+    std::unordered_map<int,std::string> arrowMap =
+    {
+        {2,u8"上"},
+        {6,u8"下"},
+        {4,u8"右"},
+        {8,u8"左"},
+    };
+    int posList[] = {0, 0, -width, 0, 1, 0, width, 0, -1, 0};
+    const char noDiagCheckAll[] = {2, 4, 6, 8, 0};
+    const char* noDiagcheckPattern[] = {noDiagCheckAll};
 
 
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
             int pos = y * width + x;
-            char buf[256];
-            sprintf(buf,"%s",flowBlocks[pos].integration > 60000 ? "x" : std::to_string(flowBlocks[pos].integration).c_str());
-            auto label_1 = PLabel::createWithTTF(buf, "fonts/SiYuanSongTi.otf", 18);
-            label_1->setPosition(pt1 + Vec2(16.f,-16.f));
-            label_1->setTextColor(Color4B::BLACK);
-            heatDebugNode->addChild(label_1);
+            int heatVal = flowBlocks[pos].integration;
+
+
+            if(heatVal == 65535)
+            {
+                continue;
+            }
+
+            int checkIndex = 0;
+            const char* checkList = noDiagcheckPattern[checkIndex];
+
+            int i = 0;
+
+            int tempCost = 100;  //越低越优先
+            while(checkList[i] != 0)
+            {
+                int newPos = pos + posList[checkList[i]];
+
+                //--- 防越界
+                if(newPos < 0 || newPos >= width * height)
+                {
+                    i++;
+                    continue;
+                }
+                //
+
+                //---
+                int newHeatVal = flowBlocks[pos + posList[checkList[i]]].integration;
+                auto newBlock = &mapBlocks[newPos];
+                int newBlockCost = costMap[static_cast<int>(newBlock->blockType)];
+                //
+
+                // 新的热力值 < 旧的热力值 ||  热力值 同等时 优先走 格子花销低的
+                if(newHeatVal < heatVal || (newHeatVal == heatVal && newBlockCost <= tempCost))
+                {
+                    flowBlocks[pos].flow = checkList[i];
+                    heatVal = newHeatVal;
+                    tempCost = newBlockCost;
+                }
+                i++;
+            }
 
         }
     }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Classes/GameMap.h
+++ Classes/GameMap.h
@@ -8,77 +8,91 @@
 
 USING_NS_CC;
 
-class GameMap : public Layer{
-
-enum class BlockType : uint8_t {
-    MAP_VOID,
-    MAP_POS,
-};
-
-
-
-union XYCheck {
-    uint32_t value;
-    struct {
-        int16_t x;
-        int16_t y;
+class GameMap : public Layer {
+    enum class BlockType : uint8_t {
+        MAP_VOID,
+        MAP_POS,
+        MAP_ROAD,
+        MAP_GLASS,
     };
 
-    // XYCheck() : value(0) {}
-    // XYCheck(int xIn, int yIn) : x(xIn), y(yIn) {}
-    // XYCheck(uint32_t valueIn) : value(valueIn) {}
-    // XYCheck(XYCheck& valueIn) : value(valueIn.value) {}
-    // operator uint32_t() const { return value; }
-};
 
-union HeatFront {
-    uint64_t value;
-    struct {
-        uint32_t heat;
-        XYCheck xy;
+    union XYCheck {
+        uint32_t value;
+
+        struct {
+            int16_t x;
+            int16_t y;
+        };
+
+        // XYCheck() : value(0) {}
+        // XYCheck(int xIn, int yIn) : x(xIn), y(yIn) {}
+        // XYCheck(uint32_t valueIn) : value(valueIn) {}
+        // XYCheck(XYCheck& valueIn) : value(valueIn.value) {}
+        // operator uint32_t() const { return value; }
     };
 
-    HeatFront() : value(0) {}
-    HeatFront(uint64_t valueIn) : value(valueIn) {}
-    HeatFront(HeatFront& valueIn) : value(valueIn.value) {}
-    operator uint64_t() const { return value; }
-};
+    union HeatFront {
+        uint64_t value;
+
+        struct {
+            uint32_t heat;
+            XYCheck xy;
+        };
+
+        HeatFront() : value(0) {
+        }
+
+        HeatFront(uint64_t valueIn) : value(valueIn) {
+        }
+
+        HeatFront(HeatFront& valueIn) : value(valueIn.value) {
+        }
+
+        operator uint64_t() const { return value; }
+    };
 
 
-struct FlowBlock {
-    uint32_t integration : 24;
-    uint32_t flow : 8;
-};
+    struct FlowBlock {
+        uint32_t integration: 24;
+        uint32_t flow: 8;
+    };
 
 
-struct MAPBLOCK {
-    uint8_t inpassable : 1;
-    BlockType blockType : 6;
-    uint8_t lock : 1;
+    struct MAPBLOCK {
+        uint8_t inpassable: 1;
+        BlockType blockType: 6;
+        uint8_t cost;
+        // MAPBLOCK(const uint8_t i) : inpassable(i){};
+    };
 
-    // MAPBLOCK(const uint8_t i) : inpassable(i){};
-};
-
-struct MAPBLOCK1 {
-    std::string s;
-    int a;
-    int c;
-    uint8_t d;
-};
+    struct MAPBLOCK1 {
+        std::string s;
+        int a;
+        int c;
+        uint8_t d;
+    };
 
 public:
     virtual bool init();
 
     void setBlocks();
 
-void bindEvents();
+    void bindEvents();
 
-MAPBLOCK* getMapBlock(int x, int y);
+    void initUI();
+
+    MAPBLOCK* getMapBlock(int x, int y);
+
     void drawMap();
 
-void clearHeatMap();
+    void clearHeatMap();
 
-void generateHeatMap(int startX, int startY);
+    void drawHeatMap();
+
+    void generateHeatMap(int startX, int startY);
+
+    void generateVectorMap(int id);
 
     CREATE_FUNC(GameMap);
 
@@ -91,8 +105,8 @@ private:
 
     FlowBlock* flowBlocks;
     DrawNode* tileIndicator;
+    DrawNode* debugDrawNode;
     Node* heatDebugNode;
-
 };
 
 #endif //GAMEMAP_H
